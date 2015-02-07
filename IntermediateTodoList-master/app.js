dfae4a81f706ae2f435864382f3a9202db41b3ee
sheyanne <sheyanne@gmail.com>
2015-02-07 15:27:05 -0800

    Adds delete and sort support

--- IntermediateTodoList-master/app.js
+++ IntermediateTodoList-master/app.js
@@ -18,7 +18,11 @@ var loadRequest = $.ajax({
 
 loadRequest.done(function(dataFromServer) {
   var itemsData = dataFromServer.items
-
+  itemsData.sort(function(a,b){
+		var newAdate = new Date(a.updated_at).getTime()
+		var newBdate = new Date(b.updated_at).getTime()
+		return newBdate - newAdate
+	})
   itemsData.forEach(function(itemData) {
     addItemToPage(itemData)
   })
@@ -34,6 +38,7 @@ $('#add-form').on('submit', function(event) {
    })
 
   creationRequest.done(function(itemDataFromServer) {
+		$('#create').val('')
     addItemToPage(itemDataFromServer)
   })
 })
@@ -55,4 +60,34 @@ $('#list').on('click', '.complete-button', function(event) {
 	  }
 	})
 })
+
+$('#list').on('click', '.delete-button', function(event) {
+  var item = $(event.target).parent()
+  var itemId = item.attr('data-id')
+	var deleteRequest = $.ajax({
+	  type: 'DELETE',
+	  url: "https://listalous.herokuapp.com/lists/sheyanne/items/" + itemId,
+	})
+	deleteRequest.done(function(itemData) {
+		//alert('Item ' + itemId + 'deleted')
+		item.remove()
+	})
+})
+
+$('#list').on('keypress', '.description', function(event) {
+	if (event.which === 13) {
+		event.preventDefault()
+  	var item = $(event.target).parent()
+  	var itemId = item.attr('data-id')
+		var description = item.find('.description').html()
+		var updateRequest = $.ajax({
+		  type: 'PUT',
+		  url: "https://listalous.herokuapp.com/lists/sheyanne/items/" + itemId,
+		  data: { description: description }
+		})
+		updateRequest.done(function(itemData) {
+			item.find('.description').blur()
+		})
+	} 
+}) 
 
